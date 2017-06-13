86d097c26d5859a2064423b12918dd6833407c81
Vsevolod Iursha <vsevolod.yursha@yandex.ru>
2017-06-13 18:03:57 +0300

    Done4
    Выполнено задание 4

--- Pr3/Program.cs
+++ Pr3/Program.cs
@@ -28,15 +28,30 @@ namespace Pr3
             Console.WriteLine(f);
 
             //откуда я должен черпать информацию о том, как конвертировать массив байт в целое число, если этого нет в лекционном материале??? как и работы с объектами, укпаковкой и распаковкой?
-            
-            
-                Console.WriteLine("Введите входное значение  ");
+
+            byte[] bytes = { 0, 0, 0, 25 };
+
+            // If the system architecture is little-endian (that is, little end first),
+            // reverse the byte array.
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+
+            int g = BitConverter.ToInt32(bytes, 0);
+            Console.WriteLine("int: {0}", i);
+            // Output: int: 25
+
+            Console.WriteLine("Введите входное значение  ");
                 string h = Console.ReadLine();
                 int vhod_znach = Convert.ToInt32(h);
                 Console.Write(vhod_znach);
                 Console.Read();
-            
-        
+
+           /* Console.WriteLine("Введите входное значение  ");
+            string s = Console.ReadLine();
+            int vhod = Convert.ToInt16(s);
+            Console.Write(vhod_znach);
+            Console.Read();*/
+
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
