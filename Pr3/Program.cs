f10f6fcb9cea6b699c47f20bc299d01cbd89d13a
Vsevolod Iursha <vsevolod.yursha@yandex.ru>
2017-06-13 17:57:41 +0300

    Change5
    Иправлено задание 5

--- Pr3/Program.cs
+++ Pr3/Program.cs
@@ -28,52 +28,15 @@ namespace Pr3
             Console.WriteLine(f);
 
             //откуда я должен черпать информацию о том, как конвертировать массив байт в целое число, если этого нет в лекционном материале??? как и работы с объектами, укпаковкой и распаковкой?
-
-            int numVal = -1;
-            bool repeat = true;
-
-            while (repeat)
-            {
-                Console.WriteLine("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive).");
-
-                string input = Console.ReadLine();
-
-                // ToInt32 can throw FormatException or OverflowException.
-                try
-                {
-                    numVal = Convert.ToInt32(input);
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("Input string is not a sequence of digits.");
-                }
-                catch (OverflowException e)
-                {
-                    Console.WriteLine("The number cannot fit in an Int32.");
-                }
-                finally
-                {
-                    if (numVal < Int32.MaxValue)
-                    {
-                        Console.WriteLine("The new value is {0}", numVal + 1);
-                    }
-                    else
-                    {
-                        Console.WriteLine("numVal cannot be incremented beyond its current value");
-                    }
-                }
-                Console.WriteLine("Go again? Y/N");
-                string go = Console.ReadLine();
-                if (go == "Y" || go == "y")
-                {
-                    repeat = true;
-                }
-                else
-                {
-                    repeat = false;
-                }
-            }
-
+            
+            
+                Console.WriteLine("Введите входное значение  ");
+                string h = Console.ReadLine();
+                int vhod_znach = Convert.ToInt32(h);
+                Console.Write(vhod_znach);
+                Console.Read();
+            
+        
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
